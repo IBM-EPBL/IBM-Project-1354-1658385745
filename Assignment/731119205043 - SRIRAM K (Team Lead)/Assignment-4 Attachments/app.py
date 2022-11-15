895757ac273fa1764b49189b5560c16a09ed8856
SRIRAM K <106462938+sriramrmx@users.noreply.github.com>
2022-11-15 23:18:23 +0530

    Update app.py

--- Assignment/731119205043 - SRIRAM K (Team Lead)/Assignment-4 Attachments/app.py
+++ Assignment/731119205043 - SRIRAM K (Team Lead)/Assignment-4 Attachments/app.py
@@ -3,7 +3,7 @@ import ibm_db
 import re
 app=Flask(__name__)
 app.secret_key = 'a'
-conn=ibm_db.connect("DATABASE=bludb;HOSTNAME=ba99a9e6-d59e-4883-8fc0-d6a8c9f7a08f.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=31321;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=rkk02660;PWD=MjT4FnHjLsN6rpbn",' ',' ')
+conn=ibm_db.connect("DATABASE=bludb;HOSTNAME=ba99a9e6-d59e-4883-8fc0-d6a8c9f7a08f.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=31321;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=**********;PWD=***********",' ',' ')
 
 @app.route('/')
 def home():
